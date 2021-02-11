31e668408f6be08b0782eb9a5f6cb6d19a7458e5
ABDIRASHID NUUR <78458279+abdinuur10@users.noreply.github.com>
2021-02-11 14:37:02 -0800

    DONE

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@
 
 1. Start creating three prompts, the first two for the two numbers and the second one for the operation
 2. Also declare another variable for the result.
-3. Using conditional statement, check if the operation is addition, subtraction, division or multipilcation
+3. Using conditional statement, check if the git operation is addition, subtraction, division or multipilcation
 4. Add the output to the result variable you created.
 5. Console.log the result.
 
